cbae84f636bb5ad1e52af3c9807aa8ce2ac443c2
deon-gracias <deongracias1@gmail.com>
2022-06-15 12:42:11 +0530

    Added more options

--- re_files.sh
+++ re_files.sh
@@ -29,29 +29,53 @@ underscore_to_space() {
     done
 }
 
-echo -n " 1 -> Space to Underscore
- 2 -> Underscore to Space
- 3 -> Uppercase to Lowercase
- 4 -> Lowercase to Uppercase
+underscore_to_hyphen() {
+    for file in * ; do
+        new_file=`echo $file | tr "_" "-"`
+        echo $new_file
+        mv "$file" "$new_file"
+    done
+}
+
+space_to_hyphen() {
+    for file in * ; do
+        new_file=`echo $file | tr " " "-"`
+        echo $new_file
+        mv "$file" "$new_file"
+    done
+}
+
+echo -n "
+ 1 -> Uppercase to Lowercase
+ 2 -> Lowercase to Uppercase
+ 3 -> Space to Underscore
+ 4 -> Underscore to Space
+ 5 -> Underscore to Hyphen
+ 6 -> Space to Hyphen
  => "
 read opt
 
 case $opt in
     1)
-        space_to_underscore
-        ;;
-    2)
-        underscore_to_space
-        ;;
-
-    3)
         uppercase_to_lowercase
         ;;
-
-    4)
+    2)
         lowercase_to_uppercase
         ;;
 
+    3)
+        space_to_underscore
+        ;;
+
+    4)
+        underscore_to_space
+        ;;
+    5)
+        underscore_to_hyphen
+        ;;
+    6)
+        space_to_hyphen
+        ;;
     *)
         echo "Invalid Option"
         ;;
